592157b049982bc5f6ea86b6a33f1a44e105e6ff
cwei01 <62791556+cwei01@users.noreply.github.com>
2020-04-19 12:46:27 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import re
+action=['a','b']
+action[0]='s'
+print(action)
+#num=re.findall(r'[0-9]+|[A-Z]',action)
+#print(num)
+# tag=re.findall(r'[a-z]',action)
+# # ch=ord(ch)-64
+# # num=int(num)
+# print(tag)
+#b=(ch-1,num-1)
+
+# print(b)
+# position=(1,2)
+# x=position[0]
+# #y=position(1)
+# print(x)
+# from numpy.ma import count
+#
+# tag="f A1"
+# print(len(tag))
